e1cbdc204ec352b9cfb5bf019796973e0f1107bc
wowdabug <110950607+wowdabug@users.noreply.github.com>
2026-08-16 13:55:12 -0500

    featured game

--- main.js
+++ main.js
@@ -1,270 +1,333 @@
 const searchInput = document.getElementById("search-input");
 const searchList = document.getElementById("search-list");
+const gameContainer = document.getElementById("game-container");
 
 const games = [
     {
         name: "Adventure",
-        path: "adventure.a26",
+        id: "adventure",
+        ext: ".a26",
         core: "atari2600"
     },
     {
         name: "Banjo-Kazooie",
-        path: "banjo_kazooie.n64",
+        id: "banjo_kazooie",
+        ext: ".n64",
         core: "n64"
     },
     {
         name: "Bomberman",
-        path: "bomberman.nes",
+        id: "bomberman",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Breakout",
-        path: "breakout.bin",
+        id: "breakout",
+        ext: ".bin",
         core: "atari2600"
     },
     {
         name: "Donkey Kong",
-        path: "donkey_kong.zip",
+        id: "donkey_kong",
+        ext: ".zip",
         core: "arcade"
     },
     {
         name: "Donkey Kong 64",
-        path: "donkey_kong_64.z64",
+        id: "donkey_kong_64",
+        ext: ".z64",
         core: "n64"
     },
     {
         name: "Donkey Kong Country",
-        path: "donkey_kong_country.sfc",
+        id: "donkey_kong_country",
+        ext: ".sfc",
         core: "snes"
     },
     {
         name: "Dr. Mario (GB)",
-        path: "dr_mario.gb",
+        id: "dr_mario",
+        ext: ".gb",
         core: "gb"
     },
     {
         name: "Dr. Mario (NES)",
-        path: "dr_mario.nes",
+        id: "dr_mario",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Frogger",
-        path: "frogger.zip",
+        id: "frogger",
+        ext: ".zip",
         core: "arcade"
     },
     {
         name: "Galaga",
-        path: "galaga.zip",
+        id: "galaga",
+        ext: ".zip",
         core: "arcade"
     },
     {
         name: "Mario Bros.",
-        path: "mario_bros.nes",
+        id: "mario_bros",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Mario Kart 64",
-        path: "mario_kart_64.n64",
+        id: "mario_kart_64",
+        ext: ".n64",
         core: "n64"
     },
     {
         name: "Mega Man",
-        path: "mega_man.nes",
+        id: "mega_man",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Mega Man 2",
-        path: "mega_man_2.nes",
+        id: "mega_man_2",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Mega Man 3",
-        path: "mega_man_3.nes",
+        id: "mega_man_3",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Mega Man 4",
-        path: "mega_man_4.nes",
+        id: "mega_man_4",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Mike Tyson's Punch Out!!",
-        path: "mike_tyson_s_punch_out.nes",
+        id: "mike_tyson_s_punch_out",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Pac-Man",
-        path: "pac_man.zip",
+        id: "pac_man",
+        ext: ".zip",
         core: "arcade"
     },
     {
         name: "Paper Mario",
-        path: "paper_mario.n64",
+        id: "paper_mario",
+        ext: ".n64",
         core: "n64"
     },
     {
         name: "Pitfall!",
-        path: "pitfall.a26",
+        id: "pitfall",
+        ext: ".a26",
         core: "atari2600"
     },
     {
         name: "Pokémon Blue Version",
-        path: "pokemon_blue_version.gb",
+        id: "pokemon_blue_version",
+        ext: ".gb",
         core: "gb"
     },
     {
         name: "Pokémon Crystal Version",
-        path: "pokemon_crystal_version.gbc",
+        id: "pokemon_crystal_version",
+        ext: ".gbc",
         core: "gba"
     },
     {
         name: "Pokémon Emerald Version",
-        path: "pokemon_emerald_version.gba",
+        id: "pokemon_emerald_version",
+        ext: ".gba",
         core: "gba"
     },
     {
         name: "Pokémon FireRed Version",
-        path: "pokemon_firered_version.gba",
+        id: "pokemon_firered_version",
+        ext: ".gba",
         core: "gba"
     },
     {
         name: "Pokémon Gold Version",
-        path: "pokemon_gold_version.gbc",
+        id: "pokemon_gold_version",
+        ext: ".gbc",
         core: "gba"
     },
     {
         name: "Pokémon LeafGreen Version",
-        path: "pokemon_leafgreen_version.gba",
+        id: "pokemon_leafgreen_version",
+        ext: ".gba",
         core: "gba"
     },
     {
         name: "Pokémon Red Version",
-        path: "pokemon_red_version.gb",
+        id: "pokemon_red_version",
+        ext: ".gb",
         core: "gb"
     },
     {
         name: "Pokémon Silver Version",
-        path: "pokemon_silver_version.gbc",
+        id: "pokemon_silver_version",
+        ext: ".gbc",
         core: "gba"
     },
     {
         name: "Pokémon Yellow Version: Special Pikachu Edition",
-        path: "pokemon_yellow_version_special_pikachu_edition.gbc",
+        id: "pokemon_yellow_version_special_pikachu_edition",
+        ext: ".gbc",
         core: "gb"
     },
     {
         name: "Sonic Advance",
-        path: "sonic_advance.gba",
+        id: "sonic_advance",
+        ext: ".gba",
         core: "gba"
     },
     {
         name: "Sonic & Knuckles",
-        path: "sonic_knuckles.bin",
+        id: "sonic_knuckles",
+        ext: ".bin",
         core: "segaMD"
     },
     {
         name: "Sonic the Hedgehog",
-        path: "sonic_the_hedgehog.bin",
+        id: "sonic_the_hedgehog",
+        ext: ".bin",
         core: "segaMD"
     },
     {
         name: "Sonic the Hedgehog 2",
-        path: "sonic_the_hedgehog_2.bin",
+        id: "sonic_the_hedgehog_2",
+        ext: ".bin",
         core: "segaMD"
     },
     {
         name: "Sonic the Hedgehog 3",
-        path: "sonic_the_hedgehog_3.bin",
+        id: "sonic_the_hedgehog_3",
+        ext: ".bin",
         core: "segaMD"
     },
     {
         name: "Space Invaders",
-        path: "space_invaders.zip",
+        id: "space_invaders",
+        ext: ".zip",
         core: "arcade"
     },
     {
         name: "Super Mario 64",
-        path: "super_mario_64.z64",
+        id: "super_mario_64",
+        ext: ".z64",
         core: "n64"
     },
     {
         name: "Super Mario All-Stars",
-        path: "super_mario_all_stars.smc",
+        id: "super_mario_all_stars",
+        ext: ".smc",
         core: "snes"
     },
     {
         name: "Super Mario Bros.",
-        path: "super_mario_bros.nes",
+        id: "super_mario_bros",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Super Mario Bros. 2",
-        path: "super_mario_bros_2.nes",
+        id: "super_mario_bros_2",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Super Mario Bros. 3",
-        path: "super_mario_bros_3.nes",
+        id: "super_mario_bros_3",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "Super Mario World",
-        path: "super_mario_world.smc",
+        id: "super_mario_world",
+        ext: ".smc",
         core: "snes"
     },
     {
         name: "Super Mario World 2: Yoshi's Island",
-        path: "super_mario_world_2_yoshi_s_island.sfc",
+        id: "super_mario_world_2_yoshi_s_island",
+        ext: ".sfc",
         core: "snes"
     },
     {
         name: "Super Smash Bros.",
-        path: "super_smash_bros.n64",
+        id: "super_smash_bros",
+        ext: ".n64",
         core: "n64"
     },
     {
         name: "Tetris (GB)",
-        path: "tetris.gb",
+        id: "tetris",
+        ext: ".gb",
         core: "gb"
     },
     {
         name: "Tetris (NES)",
-        path: "tetris.nes",
+        id: "tetris",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "The Legend of Zelda",
-        path: "the_legend_of_zelda.nes",
+        id: "the_legend_of_zelda",
+        ext: ".nes",
         core: "nes"
     },
     {
         name: "The Legend of Zelda: A Link to the Past",
-        path: "the_legend_of_zelda_a_link_to_the_past.sfc",
+        id: "the_legend_of_zelda_a_link_to_the_past",
+        ext: ".sfc",
         core: "snes"
     },
     {
         name: "The Legend of Zelda: Majora's Mask",
-        path: "the_legend_of_zelda_majora_s_mask.n64",
+        id: "the_legend_of_zelda_majora_s_mask",
+        ext: ".n64",
         core: "n64"
     },
     {
         name: "The Legend of Zelda: Ocarina of Time",
-        path: "the_legend_of_zelda_ocarina_of_time.n64",
+        id: "the_legend_of_zelda_ocarina_of_time",
+        ext: ".n64",
         core: "n64"
     }
 ];
 
-function loadGame(index) {
+function init() {
+    const paramsString = window.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+
+    const id = searchParams.get("id");
+    loadGame(id);
+}
+
+function loadGame(id) {
     const local = true;
 
-    const game = games[index];
+    const game = games.find(game => game.id == id);
+    if (!game) {
+        return
+    }
+
     const cdn = window.cdn || (local ? "data/" : "https://cdn.emulatorjs.org/stable/data/");
     const core = `${game.core}`;
 
     window.EJS_player = "#game";
     window.EJS_gameName = game.name;
     window.EJS_biosUrl = "";
-    window.EJS_gameUrl = `roms/${game.path}`;
+    window.EJS_gameUrl = `roms/${game.id}${game.ext}`;
     window.EJS_core = core;
     window.EJS_pathtodata = cdn;
     window.EJS_startOnLoaded = true;
@@ -278,11 +341,15 @@ function loadGame(index) {
         window.EJS_threads = true;
     }
     
+    gameContainer.innerHTML = "";
+    const div = document.createElement("div");
+    div.id = "game";
+    gameContainer.appendChild(div);
     const script = document.createElement("script");
     script.src = cdn + "loader.js";
     document.body.appendChild(script);
 
-    console.log(name);
+    console.log(`Loaded game: ${game.name}`);
 }
 
 function activateSearch() {
@@ -298,7 +365,7 @@ function deactivateSearch() {
 
 searchInput.addEventListener("focus", (event) => {
     searchList.innerHTML = games
-        .map((item, index) => `<div id="${index}" class="search-list-item">${item.name}</div>`)
+        .map((item) => `<div id="${item.id}" class="search-list-item">${item.name}</div>`)
         .join("");
     activateSearch();
 });
@@ -317,7 +384,7 @@ searchInput.addEventListener("input", (event) => {
 
     if (matches.length > 0) {
         searchList.innerHTML = matches
-            .map((item, index) => `<div id="${index}" class="search-list-item">${item.name}</div>`)
+            .map((item) => `<div id="${item.id}" class="search-list-item">${item.name}</div>`)
             .join("");
         activateSearch();
     } else {
@@ -332,3 +399,5 @@ searchList.addEventListener("mousedown", (event) => {
         loadGame(item.id);
     }
 });
+
+init();
