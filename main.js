a011ede75e751435146dfb210d3655c75af7f6be
wowdabug <110950607+wowdabug@users.noreply.github.com>
2026-08-16 14:54:51 -0500

    random featured game + json refactoring

--- main.js
+++ main.js
@@ -2,316 +2,7 @@ const searchInput = document.getElementById("search-input");
 const searchList = document.getElementById("search-list");
 const gameContainer = document.getElementById("game-container");
 
-const games = [
-    {
-        name: "Adventure",
-        id: "adventure",
-        ext: ".a26",
-        core: "atari2600"
-    },
-    {
-        name: "Banjo-Kazooie",
-        id: "banjo_kazooie",
-        ext: ".n64",
-        core: "n64"
-    },
-    {
-        name: "Bomberman",
-        id: "bomberman",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Breakout",
-        id: "breakout",
-        ext: ".bin",
-        core: "atari2600"
-    },
-    {
-        name: "Donkey Kong",
-        id: "donkey_kong",
-        ext: ".zip",
-        core: "arcade"
-    },
-    {
-        name: "Donkey Kong 64",
-        id: "donkey_kong_64",
-        ext: ".z64",
-        core: "n64"
-    },
-    {
-        name: "Donkey Kong Country",
-        id: "donkey_kong_country",
-        ext: ".sfc",
-        core: "snes"
-    },
-    {
-        name: "Dr. Mario (GB)",
-        id: "dr_mario",
-        ext: ".gb",
-        core: "gb"
-    },
-    {
-        name: "Dr. Mario (NES)",
-        id: "dr_mario",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Frogger",
-        id: "frogger",
-        ext: ".zip",
-        core: "arcade"
-    },
-    {
-        name: "Galaga",
-        id: "galaga",
-        ext: ".zip",
-        core: "arcade"
-    },
-    {
-        name: "Mario Bros.",
-        id: "mario_bros",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Mario Kart 64",
-        id: "mario_kart_64",
-        ext: ".n64",
-        core: "n64"
-    },
-    {
-        name: "Mega Man",
-        id: "mega_man",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Mega Man 2",
-        id: "mega_man_2",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Mega Man 3",
-        id: "mega_man_3",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Mega Man 4",
-        id: "mega_man_4",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Mike Tyson's Punch Out!!",
-        id: "mike_tyson_s_punch_out",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Pac-Man",
-        id: "pac_man",
-        ext: ".zip",
-        core: "arcade"
-    },
-    {
-        name: "Paper Mario",
-        id: "paper_mario",
-        ext: ".n64",
-        core: "n64"
-    },
-    {
-        name: "Pitfall!",
-        id: "pitfall",
-        ext: ".a26",
-        core: "atari2600"
-    },
-    {
-        name: "Pokémon Blue Version",
-        id: "pokemon_blue_version",
-        ext: ".gb",
-        core: "gb"
-    },
-    {
-        name: "Pokémon Crystal Version",
-        id: "pokemon_crystal_version",
-        ext: ".gbc",
-        core: "gba"
-    },
-    {
-        name: "Pokémon Emerald Version",
-        id: "pokemon_emerald_version",
-        ext: ".gba",
-        core: "gba"
-    },
-    {
-        name: "Pokémon FireRed Version",
-        id: "pokemon_firered_version",
-        ext: ".gba",
-        core: "gba"
-    },
-    {
-        name: "Pokémon Gold Version",
-        id: "pokemon_gold_version",
-        ext: ".gbc",
-        core: "gba"
-    },
-    {
-        name: "Pokémon LeafGreen Version",
-        id: "pokemon_leafgreen_version",
-        ext: ".gba",
-        core: "gba"
-    },
-    {
-        name: "Pokémon Red Version",
-        id: "pokemon_red_version",
-        ext: ".gb",
-        core: "gb"
-    },
-    {
-        name: "Pokémon Silver Version",
-        id: "pokemon_silver_version",
-        ext: ".gbc",
-        core: "gba"
-    },
-    {
-        name: "Pokémon Yellow Version: Special Pikachu Edition",
-        id: "pokemon_yellow_version_special_pikachu_edition",
-        ext: ".gbc",
-        core: "gb"
-    },
-    {
-        name: "Sonic Advance",
-        id: "sonic_advance",
-        ext: ".gba",
-        core: "gba"
-    },
-    {
-        name: "Sonic & Knuckles",
-        id: "sonic_knuckles",
-        ext: ".bin",
-        core: "segaMD"
-    },
-    {
-        name: "Sonic the Hedgehog",
-        id: "sonic_the_hedgehog",
-        ext: ".bin",
-        core: "segaMD"
-    },
-    {
-        name: "Sonic the Hedgehog 2",
-        id: "sonic_the_hedgehog_2",
-        ext: ".bin",
-        core: "segaMD"
-    },
-    {
-        name: "Sonic the Hedgehog 3",
-        id: "sonic_the_hedgehog_3",
-        ext: ".bin",
-        core: "segaMD"
-    },
-    {
-        name: "Space Invaders",
-        id: "space_invaders",
-        ext: ".zip",
-        core: "arcade"
-    },
-    {
-        name: "Super Mario 64",
-        id: "super_mario_64",
-        ext: ".z64",
-        core: "n64"
-    },
-    {
-        name: "Super Mario All-Stars",
-        id: "super_mario_all_stars",
-        ext: ".smc",
-        core: "snes"
-    },
-    {
-        name: "Super Mario Bros.",
-        id: "super_mario_bros",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Super Mario Bros. 2",
-        id: "super_mario_bros_2",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Super Mario Bros. 3",
-        id: "super_mario_bros_3",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "Super Mario World",
-        id: "super_mario_world",
-        ext: ".smc",
-        core: "snes"
-    },
-    {
-        name: "Super Mario World 2: Yoshi's Island",
-        id: "super_mario_world_2_yoshi_s_island",
-        ext: ".sfc",
-        core: "snes"
-    },
-    {
-        name: "Super Smash Bros.",
-        id: "super_smash_bros",
-        ext: ".n64",
-        core: "n64"
-    },
-    {
-        name: "Tetris (GB)",
-        id: "tetris",
-        ext: ".gb",
-        core: "gb"
-    },
-    {
-        name: "Tetris (NES)",
-        id: "tetris",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "The Legend of Zelda",
-        id: "the_legend_of_zelda",
-        ext: ".nes",
-        core: "nes"
-    },
-    {
-        name: "The Legend of Zelda: A Link to the Past",
-        id: "the_legend_of_zelda_a_link_to_the_past",
-        ext: ".sfc",
-        core: "snes"
-    },
-    {
-        name: "The Legend of Zelda: Majora's Mask",
-        id: "the_legend_of_zelda_majora_s_mask",
-        ext: ".n64",
-        core: "n64"
-    },
-    {
-        name: "The Legend of Zelda: Ocarina of Time",
-        id: "the_legend_of_zelda_ocarina_of_time",
-        ext: ".n64",
-        core: "n64"
-    }
-];
-
-function init() {
-    const paramsString = window.location.search;
-    const searchParams = new URLSearchParams(paramsString);
-
-    const id = searchParams.get("id");
-    loadGame(id);
-}
+let games;
 
 function loadGame(id) {
     const local = true;
@@ -361,6 +52,71 @@ function deactivateSearch() {
     searchList.style.display = "none";
     searchList.innerHTML = "";
     searchInput.style.borderRadius = "2px";
+}
+
+function initFeatured() {
+    const featuredGames = [
+        "galaga",
+        "super_mario_bros",
+        "pacman",
+        "super_mario_64",
+        "pokemon_red_version",
+        "sonic_the_hedgehog",
+        "tetris_nes"
+    ];
+
+    const method = "random";
+
+    let day = 0;
+    switch (method) {
+        case "date":
+            const date = new Date();
+            day = date.getDay();
+            break;
+        case "random":
+            day = Math.floor(Math.random() * 7);
+            break;
+    }
+
+    const game = games.find(game => game.id == featuredGames[day]);
+    if (!game) {
+        throw new Error(`couldn't find featured game with id ${featuredGames[day]}`);
+    }
+
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const a = document.createElement("a");
+
+    img.src = `assets/${game.id}.png`;
+
+    div.id = "featured-game";
+
+    p.textContent = "Featured Game: ";
+
+    a.textContent = `${game.name}`;
+    a.href = "#";
+    a.onclick = (event) => {
+        loadGame(`${game.id}`);
+    };
+    
+    p.appendChild(a);
+    div.appendChild(p);
+    div.appendChild(img);
+    gameContainer.appendChild(div);
+}
+
+async function init() {
+    const response = await fetch("games.json");
+    games = await response.json();
+
+    initFeatured();
+
+    const paramsString = window.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+
+    const id = searchParams.get("id");
+    loadGame(id);
 }
 
 searchInput.addEventListener("focus", (event) => {
